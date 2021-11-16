7724907277a07ee8df436c742d81047009381e4d
Irving Michelle Rios Castellanos <gamma-1994@hotmail.com>
2021-11-16 11:14:48 -0600

    "fixed search"

--- routes/controllers/searchPeople.js
+++ routes/controllers/searchPeople.js
@@ -5,9 +5,10 @@ const torreSearchURL = 'https://search.torre.co/people/_search';
 module.exports = searchPeople = async (req,res,next) => {
 try  { 
     const {term} = req.body;
+    console.log(term);
     let response = (await axios.post(torreSearchURL, {name: {term}})).data;
 
-    res.status(200).json(response)
+    res.status(200).json(response.results);
 }
 catch (err){
     next(err);
